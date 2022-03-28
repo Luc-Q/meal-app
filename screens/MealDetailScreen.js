8bd2935dbafa2529678c5d486b07e32dca7fbb4f
Lu <jetqin_2008@hotmail.com>
2022-03-28 18:40:19 +1100

    output content

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,30 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import MealDetail from "../components/MealDetails";
+import { MEALS } from "../data/dummy-data";
 
 function MealDetailScreen({ route }) {
     const mealId = route.params.mealId;
-    return <Text>Meal detail screen ({mealId})</Text>;
+
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+    return (
+        <View>
+            <Image source={{ uri: selectedMeal.imageUrl }} />
+            <Text>{selectedMeal.title}</Text>
+            <MealDetail
+                duration={selectedMeal.duration}
+                complexity={selectedMeal.complexity}
+                affordability={selectedMeal.affordability}
+            />
+            <Text>Ingredients</Text>
+            {selectedMeal.ingredients.map((ingredient) => (
+                <Text key={ingredient}>{ingredient}</Text>
+            ))}
+            <Text>Steps</Text>
+            {selectedMeal.steps.map((step) => (
+                <Text key={step}>{step}</Text>
+            ))}
+        </View>
+    );
 }
 
 export default MealDetailScreen;
